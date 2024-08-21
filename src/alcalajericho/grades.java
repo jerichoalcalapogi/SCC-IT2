1b1c3fa07f4ab07a11a5e828308a800b74a979c2
jerichoalcala321@gmail.com <christianyongzon143@gmail.com>
2024-08-21 14:48:56 +0800

    my first repo ohahaha

---
+++ src/alcalajericho/grades.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package alcalajericho;
+import java.util.Scanner;
+public class grades {
+      int id;
+    String name;
+    double p,m,pf,f;
+    public void addGrades(int sid, String name, double prl, double mid, double prf, double fnl){
+        
+        this.name = name;
+        this.id=sid;
+        this.p =prl;
+        this.m =mid;
+        this.pf=prf;
+        this.f =fnl;
+    }
+    
+    public void viewGrades(int id) {
+        double average= (this.p + this.m + this.pf + this.f) / 4;
+        String remarks=(average > 3.0) ? "Failed": "Passed";
+        
+        System.out.printf("%-10d %-10s 0.20 %-10.2f %-10.2f %-10.2f %-10.2f %-10.2f %-10s\n",this.id, this.name, this.p, this.m, this.pf, this.f, average, remarks);
+        
+}
+}
